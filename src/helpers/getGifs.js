6e91c359fb9750453255c12e1dbacd6cf88443ef
KennethEvolves <kennethgaeliuitrios02@gmail.com>
2024-05-04 19:17:18 -0600

    Update API_KEY

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const api_key = 'Szpa0H02VJN1vIaJ0xiXBXZPk0d2HFjC';
+    const api_key = 'hlkSRx3KG3hkkIjE1cPS0F41GsWZ84em';
     const limit = 10;
     const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${category}&limit=${limit}`;
     const resp = await fetch(url);
